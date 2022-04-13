88dc81610199688185510728db81559482e01b58
FailPr <102379340+FailPr@users.noreply.github.com>
2022-04-14 02:10:34 +0300

    Update add.php

--- scripts/php/add.php
+++ scripts/php/add.php
@@ -4,13 +4,12 @@ include 'connection.php';
 
 $Helper = $_POST['Helper'];
 $Info = $_POST['Info'];
-
 if ($Helper == "Send") 
 {
     $conn = new mysqli($servername, $username, $password, $dbname);
 
     $sql = "INSERT INTO questions (Question)
-    VALUES ('$Info')";
+    VALUES (" . json_encode($Info) . ")";
 
     if ($conn->query($sql) === TRUE) {
         echo json_encode(array("a" => 'New record created successfully'));
@@ -21,5 +20,4 @@ if ($Helper == "Send")
 
     $conn->close();
 }
-
 ?>
